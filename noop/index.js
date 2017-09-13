7b90dca3c25ac4b9ba42646114f409d020242e8d
Stephen Fluin <stephen.fluin@gmail.com>
2017-09-13 15:40:19 -0700

    Initial schematic

---
+++ noop/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+* @license
+* Copyright Google Inc. All Rights Reserved.
+*
+* Use of this source code is governed by an MIT-style license that can be
+* found in the LICENSE file at https://angular.io/license
+*/
+const schematics_1 = require("@angular-devkit/schematics");
+const stringUtils = require("../strings");
+function default_1(options) {
+    return schematics_1.chain([
+
+    ]);
+}
+exports.default = default_1;
